f4623570840f6894e5299c95e0e89a69fa718dcd
Oscar <oscarcabrerarojas2023@gmail.com>
2024-11-06 22:38:04 -0600

    Documentación a nivel de clase para el archivo Sistema

--- GestionEscolar/src/controlador/Sistema.java
+++ GestionEscolar/src/controlador/Sistema.java
@@ -1,8 +1,11 @@
 package controlador;
 
 /**
- * Documentación de la clase Sistema.
- * Comentario sobre el comentario.
+ * Clase representante del controlador para el resto de la lógica del programa.
+ * 
+ * El Sistema se encarga de gestionar el flujo de peticiones que realiza el resto del programa para la interacción con la información en el modelo, y los requerimientos de la vista.
+ * 
+ * El resto de clases y módulos existentes en el controlador de la aplicación podrán ser únicamente accesibles por el Sistema.
  *
  * @author Oscar Rojas
  */
