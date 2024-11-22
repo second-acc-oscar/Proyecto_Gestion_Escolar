298599edfbccd34ef603295baa86ac1b8aade4d4
Oscar <oscarcabrerarojas2023@gmail.com>
2024-11-22 04:14:50 -0600

    Documentación

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controlador"},{"l":"controlador.crud_academico_controller"},{"l":"controlador.crud_alumno_controller"},{"l":"modelo"},{"l":"modelo.AppClasses"},{"l":"modelo.data_base"},{"l":"vista"},{"l":"vista.crud_academico"},{"l":"vista.crud_alumno"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controlador"},{"l":"modelo"},{"l":"modelo.AppClasses"},{"l":"vista"}];updateSearchResults();
